a1fdd8ba0e57087b7057e2042fa1d393eec1e5f4
Calvin Donner <calvinjdonner@gmail.com>
2021-10-10 10:29:36 -0600

    combined files

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db.json');
+const { note } = require('./db/db.json');
+
+const util = require('util');
+const fs = require('fs');
+const uuid = require('uuid').v1;
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,4 +20,42 @@ app.use('/', htmlRoutes);
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+class Server {
+    read() {
+        return readFileAsync('db/db.json', 'utf8')
+    }
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note))
+    }
+
+    addNote(note) {
+        const { title, text } = note
+
+        if(!title || !text) {
+            throw new Error('Title and Text cannot be empty')
+        }
+
+        const newNote = { title, text, id: uuid() }
+
+        return this.getNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => this.newNote)
+    }
+
+    getNotes() {
+        return this.read()
+            .then(notes => {
+                return JSON.parse(notes) || [];
+            })
+    }
+    removeNote(id) {
+        return this.getNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(keptNotes => this.write(keptNotes))
+    }
+}
+
+module.exports = new Server();
 
